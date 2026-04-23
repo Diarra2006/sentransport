83f869e176e2a22c19ba90742e70760e2ecb1c82
diarradia1977-hub <diarradia1977@gmail.com>
2026-04-23 19:25:22 +0000

    Exercices 1 a 3 .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
+import Statistique from './Statistique';
 
 function App() {
   return (
@@ -8,7 +10,13 @@ function App() {
       <main className="contenu">
       <h1>Bienvenue sur SenTransport</h1>
       <p>Le transport en commun a Dakar</p>
+      <section className='Stat-container'>
+          <Statistique />
+          <Statistique />
+          <Statistique />
+        </section>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+    return(
+        <statistique className='statique'>
+            <h2 className='Stat-chiffre'>10</h2>
+            <p className='Stat-libelle'>Bus disponibles</p>
+    </statistique>
+    );
+}
+
+export default Statistique;
